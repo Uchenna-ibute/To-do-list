ed1ae4502f18ceb67006b04c20390f0fec9e9ac3
Uche Ibute <uchennaoluchi49@gmail.com>
2023-01-19 07:33:07 +0100

    Fixing the errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
