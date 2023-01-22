f2795eb15de079644d5567cb2974d5b3cf50ab2b
Uche Ibute <uchennaoluchi49@gmail.com>
2023-01-22 09:55:15 +0100

    Fixing the errors

--- src/module/form.js
+++ src/module/form.js
@@ -28,7 +28,7 @@ const form = () => {
 
     tasks.push(task);
     list.innerHTML = '';
-    tasks.map((task) => { display(task, list)} );
+    tasks.map((task) => (display(task, list)));
     input.value = '';
     updateLocalStorage(tasks);
     MOR(tasks);
@@ -50,7 +50,7 @@ const form = () => {
             time.parentElement.parentElement.remove();
             tasks.forEach((t, m) => {
               if (n === m) {
-                tasks = tasks.filter((tee) => { tee.index !== t.index });
+                tasks = tasks.filter((tee) => ( tee.index !== t.index ));
 
                 // changeindexs
                 tasks.forEach((tep, num) => {
@@ -99,7 +99,7 @@ const form = () => {
         if (e.id === 'completed') {
           e.remove();
         }
-        tasks = tasks.filter((t) => { t.completed !== true });
+        tasks = tasks.filter((t) => ( t.completed !== true ));
         updateLocalStorage(tasks);
       });
     });
@@ -109,7 +109,7 @@ const form = () => {
   window.addEventListener('load', () => {
     const reshow = JSON.parse(localStorage.getItem('store')) || [];
     tasks.push(...tasks, ...reshow);
-    tasks.map((task) => { display(task, list) });
+    tasks.map((task) => ( display(task, list) ));
     updateLocalStorage(tasks);
     // edit task
     const pen = document.querySelectorAll('#more');
@@ -127,7 +127,7 @@ const form = () => {
             time.parentElement.parentElement.remove();
             tasks.forEach((t, m) => {
               if (n === m) {
-                tasks = tasks.filter((tee) => { tee.index !== t.index });
+                tasks = tasks.filter((tee) => ( tee.index !== t.index ));
                 // changeindexs
                 tasks.forEach((tep, num) => {
                   tep.index = num + 1;
@@ -170,10 +170,10 @@ const form = () => {
       clear.addEventListener('click', () => {
         const each = document.querySelectorAll('#completed');
         each.forEach((e) => {
-          if (e.id === 'completed'){
+          if (e.id === 'completed') {
             e.remove();
           }
-          tasks = tasks.filter((t) => { t.completed !== true });
+          tasks = tasks.filter((t) => ( t.completed !== true ));
           updateLocalStorage(tasks);
         });
       });
