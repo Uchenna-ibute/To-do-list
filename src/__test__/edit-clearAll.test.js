ee08998d30559e72cd3fa5d186a22d2c0a88c5ce
Ghulam Subbhani <gsmalik030@gmail.com>
2023-01-27 07:39:14 +0200

    Added test for clearAll

--- src/__test__/edit-clearAll.test.js
+++ src/__test__/edit-clearAll.test.js
@@ -59,3 +59,87 @@ describe("Test for Editing Tasks", () => {
   });
 });
 
+// Test for updated ToDo's status
+describe("Test for checking completed flag", () => {
+  it("Completed Task, check status complete is updated", () => {
+    let SIZE = listCatalogue.length;
+    listCatalogue.splice(0, SIZE);
+
+    document.body.innerHTML = '<div><ul class="list"></ul></div>';
+    addItem("Complete task 1");
+    addItem("Complete task 2");
+    addItem("Complete task 3");
+    SIZE = listCatalogue.length;
+
+    const task2 = document.querySelector(`#list-${SIZE - 1}`);
+    const task2Checkbox = task2.querySelector('input[name="tasks"]');
+    task2Checkbox.checked = true;
+
+    checkFunction(task2, task2Checkbox);
+    expect(listCatalogue[SIZE - 2].complete).toBeTruthy();
+  });
+
+  it("Completed Task, check localStorage is updated after checked", () => {
+    document.body.innerHTML = '<div><ul class="list"></ul></div>';
+    addItem("Complete task 4");
+    addItem("Complete task 5");
+    addItem("Complete task 6");
+
+    const SIZE = listCatalogue.length;
+
+    const task6 = document.querySelector(`#list-${SIZE}`);
+    const task6Checkbox = task6.querySelector('input[name="tasks"]');
+    task6Checkbox.checked = true;
+
+    checkFunction(task6, task6Checkbox);
+    const savedTodos = JSON.parse(localStorage.getItem("listCatalogue"));
+    expect(savedTodos[SIZE - 1].complete).toBeTruthy();
+  });
+});
+
+// Test for Clearing Completed ToDo's
+describe("Test for clearing completed ToDos's", () => {
+  it("Clear All Task, clears all completed tasks from the DOM ", () => {
+    document.body.innerHTML = '<div><ul class="list"></ul></div>';
+    addItem("Complete task 7");
+    addItem("Complete task 8");
+    addItem("Complete task 9");
+    addItem("Complete task 10");
+    addItem("Complete task 11");
+    addItem("Complete task 12");
+
+    const list = document.querySelectorAll(".list li");
+
+    // Set the odd todos to completed
+    list.forEach((todo, index) => {
+      const checkbox = todo.querySelector('input[name="tasks"]');
+      if (index % 2) checkbox.checked = true;
+      // Update the localStorage
+      checkFunction(todo, checkbox);
+    });
+
+    const checkboxAll = document.querySelectorAll('input[name="tasks"]');
+    clearItems(checkboxAll);
+    const newList = document.querySelectorAll(".list li");
+    expect(newList).toHaveLength(3);
+    expect(listCatalogue).toHaveLength(7);
+  });
+
+  it("Clear All Task, check listCatalogue has it's length reduced", () => {
+    listCatalogue.forEach((todo, index) => {
+      if (index % 2) todo.complete = true;
+    });
+    taskList.clearAll();
+    expect(listCatalogue).toHaveLength(4);
+  });
+
+  it("Clear All Task, check localStorage is updated", () => {
+    listCatalogue.forEach((todo, index) => {
+      if (index % 2 === 1) todo.complete = true;
+    });
+    taskList.clearAll();
+
+    const savedTodos = JSON.parse(localStorage.getItem("listCatalogue"));
+    expect(savedTodos).toHaveLength(2);
+  });
+});
